f5ec628990f450ecea39e8b62afc163a85d4610d
GunsAndBugs <37249607+GunsAndBugs@users.noreply.github.com>
2018-05-21 17:35:15 +0600

    Create LIS.cpp

---
+++ LIS.cpp
@@ -0,0 +1,25 @@
+#define lim  100010
+LL arr[lim];
+LL lastVal[lim];
+LL LIS_SZ = 1;
+LL LB( LL s, LL e , LL val ){
+    LL ret = 0 , mid ;
+    while( s<=e ){
+      mid = ((s+e)/2);
+      if( lastVal[mid]<=val ){
+        ret = mid;
+        s = mid + 1;
+      }
+      else e = mid - 1;
+    }
+    return ret;
+}
+
+void genLIS( LL *a ,  LL n ){
+    FOR(i,1,n) lastVal[i] = 1e18;
+    FOR(i,1,n){
+       LL upInd = LB( 1,LIS_SZ,a[i] ) + 1;
+        lastVal[upInd] = a[i];
+        LIS_SZ = max( LIS_SZ , upInd );
+    }
+}
