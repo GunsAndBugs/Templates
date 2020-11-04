b4ab1fb24b4edbe1450a37bd20ef2419c85f1b74
hasinur010 <hasinur010@gmail.com>
2020-11-04 19:29:23 +0600

    updated CHT: my own CHT code

--- Convex_hull_trick.cpp
+++ Convex_hull_trick.cpp
@@ -1,64 +1,80 @@
+/*
+Convex Hull Trick
+Basic Problem: Given N lines(y = mx + c). Slopes are given in an order(increasing or decreasing).
+for any given value of x, find the min/max y. 
+CHT machine( _decSlope, _askMin )
+machine.add( M,C ) where y = Mx + C
+machine.query( x )
+*/
 
-struct line {
-	LL m , c;
-	line() {
-		m = 0 , c = 0;
+const LL inf = 1e18;
+struct CHT{
+	vector < LL > m,c;
+	bool decSlope, askMin;
+	CHT(bool _decSlope , bool _askMin ){
+		decSlope = _decSlope;
+		askMin = _askMin;
 	}
-	line(LL _m , LL _c) {
-		m = _m; c = _c;
+	
+	bool throwLine( LL l1,LL l2,LL l3 ){
+		if(decSlope) {
+			if( askMin )
+				return (c[l2] - c[l1])*(m[l1] - m[l3]) >= (c[l3] - c[l1])*(m[l1]-m[l2]);
+			else return (c[l2] - c[l1])*(m[l1] - m[l3]) <= (c[l3] - c[l1])*(m[l1]-m[l2]);
+			
+		}
+		else{
+			if(!askMin)
+				return (c[l2] - c[l1])*(m[l1] - m[l3]) >= (c[l3] - c[l1])*(m[l1]-m[l2]);
+			else return (c[l2] - c[l1])*(m[l1] - m[l3]) <= (c[l3] - c[l1])*(m[l1]-m[l2]);
+		}
 	}
-	bool operator<(const line &arg )const {
-		return m < arg.m;
+	
+	void add( LL M , LL C ){
+		m.pb(M); c.pb(C);
+		while( sz(m) >= 3 && throwLine(sz(m)-3,sz(m)-2,sz(m)-1)){
+			m.erase(m.end() - 2);
+			c.erase(c.end() - 2);
+		}
 	}
-	LL f(LL x) {
-		return m * x + c;
-	}
-};
-
-#define CLIM 100010
-struct CHT {
-
-	line lines[ CLIM ];
-	LL sz ;
-
-	CHT() {
-		sz = 0;
-	}
-
-	bool baad( LL i1, LL i2 , LL i3 = 0 ) {
-		return 1.0 * (double)( lines[i2].c - lines[i1].c  ) * ( lines[i1].m - lines[i3].m )
-		       > 1.0 * ( lines[i3].c - lines[i1].c  ) * ( lines[i1].m - lines[i2].m );
-
-	}
-
-	void add( LL m , LL c ) {
-		lines[0] = line(m, c);
-		while ( sz >= 2 && baad(sz - 1, sz) ) sz--;
-		lines[sz + 1] = lines[0]; sz++;
-	}
-
-	LL query( LL ai ) {
-		LL l = 1 , r = sz;
-		LL ans = 1e18;
-		while (l <= r) {
-			LL m1 = l + (r - l) / 3;
-			LL m2 = r - (r - l) / 3;
-			LL y1 = lines[m1].f(ai);
-			LL y2 = lines[m2].f(ai);
-			if (y1 <= y2) {
-				ans = y1;
+	
+	LL f(LL i,LL x){
+		return m[i]*x + c[i];
+	} 
+	
+	LL query( LL x ){
+		LL l = 0 ,r = sz(m)-1, ret;
+		if(askMin) ret = inf;
+		else ret = -inf;
+		while(l<=r){
+			LL m1 = l + (r-l)/3;
+			LL m2 = r - (r-l)/3;
+			if( f(m1,x)==f(m2,x) ){
+				ret = f(m1,x);
 				r = m2 - 1;
-			}
-			else {
-				ans = y2;
 				l = m1 + 1;
 			}
+			else if(f(m1,x)<f(m2,x)){
+				if(askMin){
+					ret = f(m1,x);
+					r = m2 - 1;
+				}
+				else{
+					ret = f(m2,x);
+					l = m1 + 1;
+				}
+			}
+			else{
+				if(askMin){
+					ret = f(m2,x);
+					l = m1 + 1;
+				}
+				else{
+					ret = f(m1,x);
+					r = m2 - 1;
+				}
+			}
 		}
-		return ans;
+		return ret;
 	}
-
 };
-
-
-#define lim 100010
-LL dp[lim], a[lim], b[lim];
